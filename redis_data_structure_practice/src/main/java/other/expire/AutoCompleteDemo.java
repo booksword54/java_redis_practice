a066038e24e1b5fd3e87b08ef339b9192cdda34a
九渊 <1058477702@qq.com>
2024-08-14 00:45:43 +0800

    feat: 基于expire实现支持冷数据自动淘汰的网站搜索自动补全案例

---
+++ redis_data_structure_practice/src/main/java/other/expire/AutoCompleteDemo.java
@@ -0,0 +1,60 @@
+package other.expire;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.Date;
+import java.util.Set;
+
+/**
+ * 支持冷数据自动淘汰的网站搜索自动补全案例
+ */
+public class AutoCompleteDemo {
+
+    private Jedis jedis = new Jedis("127.0.0.1");
+
+    /**
+     * 搜索某个关键词
+     */
+    public void search(String keyword) {
+        char[] keywordCharArray = keyword.toCharArray();
+        StringBuilder potentialKeyword = new StringBuilder();
+        for (char keywordChar : keywordCharArray) {
+            potentialKeyword.append(keywordChar);
+            jedis.zincrby("potential_keyword::" + potentialKeyword + "::keywords",
+                          new Date().getTime(),
+                          keyword);
+            // 搜索词设置过期时间，冷数据自动淘汰
+            jedis.expire("potential_keyword::" + potentialKeyword + "::keywords", 3);
+        }
+    }
+
+    /**
+     * 获取自动补全列表
+     */
+    public Set<String> getAutoCompleteList(String potentialKeyword) {
+        return jedis.zrevrange("potential_keyword::" + potentialKeyword + "::keywords", 0, 2);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        AutoCompleteDemo demo = new AutoCompleteDemo();
+        demo.search("我爱大家");
+        demo.search("我喜欢Redis");
+        demo.search("我喜欢Google");
+        demo.search("我不太喜欢玩");
+
+        Set<String> autoCompleteList = demo.getAutoCompleteList("我");
+        // 第一次搜索推荐: [我不太喜欢玩, 我喜欢Google, 我喜欢Redis]
+        System.out.println("第一次搜索推荐: " + autoCompleteList);
+
+        autoCompleteList = demo.getAutoCompleteList("我喜");
+        // 第二次搜索推荐: [我喜欢Google, 我喜欢Redis]
+        System.out.println("第二次搜索推荐: " + autoCompleteList);
+
+        Thread.sleep(5 * 1000); // 休眠5s。冷数据过期
+
+        autoCompleteList = demo.getAutoCompleteList("我");
+        // 第三次搜索推荐: []
+        System.out.println("第三次搜索推荐: " + autoCompleteList);
+
+    }
+}
