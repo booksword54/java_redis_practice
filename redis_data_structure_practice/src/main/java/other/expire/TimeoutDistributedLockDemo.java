8d4be3200ebd28b1ed63390ee2c46760a38c13c1
九渊 <1058477702@qq.com>
2024-08-14 01:03:13 +0800

    feat: 基于pipline/watch/multi/exec实现支持身份验证功能的超时自动释放的分布式锁案例

--- redis_data_structure_practice/src/main/java/other/expire/TimeoutDistributedLockDemo.java
+++ redis_data_structure_practice/src/main/java/other/expire/TimeoutDistributedLockDemo.java
@@ -1,45 +1,61 @@
 package other.expire;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+import redis.clients.jedis.Response;
 
 /**
- * 支持超时自动释放的分布式锁案例
+ * 支持身份验证功能的超时自动释放的分布式锁案例
  */
 public class TimeoutDistributedLockDemo {
 
     private static Jedis jedis = new Jedis("127.0.0.1");
 
     /**
-     * 加锁
+     * 加锁，指定user
      */
-    public boolean lock(String key, String value, int timeout) {
-        Long result = jedis.setnx(key, value);
+    public boolean lock(String key, String user, int timeout) {
+        Long result = jedis.setnx(key, user);
         jedis.expire(key, timeout);
         return result > 0;
     }
 
     /**
-     * 解锁
+     * 解锁，需要进行身份验证
      */
-    public void unlock(String key) {
-        jedis.del(key);
+    public boolean unlock(String key, String user) {
+        String currentUser = jedis.get(key);
+        try (Pipeline pipeline = jedis.pipelined()) {
+            pipeline.watch(user);
+            if (currentUser == null || currentUser.equals("")) {
+                return true;
+            }
+            if (!currentUser.equals(user)) {
+                return false;
+            }
+            pipeline.multi();
+            pipeline.del(key);
+            pipeline.exec();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
         TimeoutDistributedLockDemo demo = new TimeoutDistributedLockDemo();
-        boolean result = demo.lock("test_lock", "test_value", 10);
-        // 第一次加锁结果: true
-        System.out.println("第一次加锁结果: " + result);
+        boolean result = demo.lock("test_lock", "张三", 10);
+        // 张三加锁的结果: true
+        System.out.println("张三加锁的结果: " + result);
 
-        result = demo.lock("test_lock", "test_value", 10);
-        // 第二次加锁结果: false
-        System.out.println("第二次加锁结果: " + result);
+        boolean unlock = demo.unlock("test_lock", "李四");
+        // 李四解锁的结果: false
+        System.out.println("李四解锁的结果: " + unlock);
 
-        Thread.sleep(12 * 1000);
-
-        result = demo.lock("test_lock", "test_value", 10);
-        // 第三次加锁结果: true
-        System.out.println("第三次加锁结果: " + result);
+        unlock = demo.unlock("test_lock", "张三");
+        // 张三解锁的结果: true
+        System.out.println("张三解锁的结果: " + unlock);
     }
 
 }
